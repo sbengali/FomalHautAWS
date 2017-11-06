d3ccba78eca852f9454720ab3b62f14d89cbddf4
sbengali <salmabengali@gmail.com>
2017-11-06 11:58:44 -0800

    Reading files directly from bucket

--- detect-faces.py
+++ detect-faces.py
@@ -1,17 +1,20 @@
 import boto3
 import json
-import glob
+from boto.s3.connection import S3Connection
 
 if __name__ == "__main__":
-    for fileName in glob.glob(r'Data\*.jpg'):
-        bucket='facial-recognition-pune'
-        client=boto3.client('rekognition')
+    conn = S3Connection()
+    bucket = conn.lookup('facial-recognition-pune')
+    for obj in bucket.list():
+        if obj.key.endswith('.jpg'):
+            fileName = obj.key
+            client = boto3.client('rekognition')
 
-        response = client.detect_faces(Image={'S3Object': {'Bucket': bucket,'Name': fileName}}, Attributes=['ALL'])
+            response = client.detect_faces(Image={'S3Object': {'Bucket': 'facial-recognition-pune', 'Name': fileName}}, Attributes=['ALL'])
 
-        print('Detected faces for ' + fileName)
-        for faceDetail in response['FaceDetails']:
-            print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
-                  + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
-            print('Here are the other attributes:')
-            print(json.dumps(faceDetail, indent=4, sort_keys=True))
+            print('Detected faces for ' + fileName)
+            for faceDetail in response['FaceDetails']:
+                print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
+                      + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
+                print('Here are the other attributes:')
+                print(json.dumps(faceDetail, indent=4, sort_keys=True))
